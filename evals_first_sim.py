4eddbbe52174899dcbcfeda9ba5b6c5c1082914e
doco <75322782+c-doco@users.noreply.github.com>
2024-07-15 22:52:20 +0200

    First results

--- evals_first_sim.py
+++ evals_first_sim.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
 #==================================PARAMETERS==========================================
@@ -6,26 +7,50 @@ import matplotlib.pyplot as plt
 large_width = 400
 np.set_printoptions(linewidth=large_width)
 
-N = 50
-k = 3
+N = 100
+k = 4**2
 t = 1000
-dt = 1/1000
-noiseVariance = 1
+dt = 1/10000
+noiseVariance = 0.1
 
 #==================================EIGENVALUES_&_EIGENVECTORS==================================
 
+def EquilibriumFunction(x):
+    ret = np.zeros(N, dtype = float)
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            if i != j:
+                sum += 1/np.abs(x[i] - x[j])
+        ret[i] = -N * (x[i]**3 - k * x[i]) + sum
+    return ret
+
+def EquilibriumFunctionJacobian(x):
+    ret = np.zeros((N,N), dtype = float)
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            if i > j:
+                ret[i][j] = -1/(x[i] - x[j])**2
+                sum += 1/(x[i] - x[j])**2
+            if i < j:
+                ret[i][j] = 1/(x[i] - x[j])**2
+                sum += -1/(x[i] - x[j])**2
+        ret[i][i] = -N * (3 * x[i]**2 - k) + sum
+    return ret
+
 def Eigens():
+    initialGuess = np.zeros(N, dtype = float)
+    for i in range(N):
+        if i < N/2:
+            initialGuess[i] = np.random.normal(-np.sqrt(k), 1)
+        else:
+            initialGuess[i] = np.random.normal(np.sqrt(k), 1)
 
-    M = np.random.normal(0, 1/2, (N,N)) + 1j * np.random.normal(0, 1/2, (N,N))                          #   EVAL0   EVAL1   EVAL2
-    M = ( M + M.conj().T ) / 2                                                                          #   EVEC0_0 EVEC1_0 EVEC2_0
-    (evals, evecs) = np.linalg.eigh(M)                                                                  #   EVEC0_1 EVEC1_1 EVEC2_1
-    evals = evals/(np.sqrt(N))                                                                          #   EVEC0_2 EVEC1_2 EVEC2_2
-    
-    formatEvecs = np.zeros((N,N), dtype=complex)                                                        #arranges eigenvectors so that eigenvector of evals[i] is evecs[i] 
-    for i in range(N):                                                                                  #instead of evecs[:,i] which is default in numpy
-        formatEvecs[i] = evecs[:,i].copy()
-
-    return (evals, evecs)
+    sol  = scipy.optimize.root(EquilibriumFunction, initialGuess, jac=EquilibriumFunctionJacobian)     
+    ret = sol.x
+    ret.sort()
+    return ret
 
 #==================================EVOLUTION_FUNCTION_&_AUXILIARY_FUNCTIONS====================================
 
@@ -61,21 +86,6 @@ def EigensEvolution(evals, evecs):
 
     return (dEvals, dEvecs)
 
-def EvalsEvolution(evals):
-    dB = np.random.normal(0, 1, N)
-    dEvals = np.zeros(N, dtype = float)
-
-    for i in range(N):
-        suma1 = 0
-        for j in range(N):
-            if i != j:
-                delta = 1/(evals[i] - evals[j])
-                suma1 += delta
-        dEvals[i] = - potentialDerivative(evals[i]) * dt + (1/N) * suma1 * dt + (1/np.sqrt(N)) * dB[i]
-
-    return dEvals
-
-
 #==================================PLOT_FUNCTIONS======================================
 
 def EigensPlot(evals, evecs):
@@ -89,29 +99,26 @@ def EigensPlot(evals, evecs):
 
     x = np.linspace(evals[0] * 1.2, evals[N-1] * 1.2,  1001)
     ax[1].plot(x, potentialV(x), color='red')
-    #ax[1].scatter(evals, [potentialV(x) for x in evals])
-    ax[1].scatter(evals, [0 for x in range(N)])
-    ax[1].scatter([np.sqrt(k), -np.sqrt(k)], [0,0])
+    ax[1].scatter(evals, [potentialV(x) for x in evals])
 
     fig.colorbar(cax)
 
 #==================================MAIN============================================
 
-evals, _ = Eigens()
-for i in range(1):
-    evals += EvalsEvolution(evals)
-
+evals = Eigens()
 evecs = np.identity(N, dtype=complex)
 
 for i in range(t):
+    print(i)
     dEvals, dEvecs = EigensEvolution(evals, evecs)
     evals += dEvals
     evecs += dEvecs
 
-    #evecs = evecs[np.argsort(evals)]             #sorts the eigenvectors by ascending eigenvalues
-    #evals = evals[np.argsort(evals)]
-    #evecs, _ = np.linalg.qr(evecs)
-
+#sorts the eigenvectors by ascending eigenvalues
+    evecs = np.array([x for _, x in sorted(zip(evals, evecs))])             
+    evals.sort()
+    evecs, _ = np.linalg.qr(np.transpose(evecs))
+    evecs = np.transpose(evecs)
 
 EigensPlot(evals, evecs)
 plt.show()
--- metropolis_simulation.py
+++ metropolis_simulation.py
@@ -1,37 +1,77 @@
 #source on Metropolis-Hastings algorithm: https://blog.djnavarro.net/posts/2023-04-12_metropolis-hastings/
 
 import numpy as np
+import scipy
 import matplotlib.pyplot as plt
 
-#===================================PARAMETERS=================================================
+#=================================PARAMETERS&GLOBAL_VARIABLES=================================
 
 large_width = 400
 np.set_printoptions(linewidth=large_width)
 
 N = 100
-k = 3
-order = int(np.sqrt(N) / 2)
+k = 2.2
+order = 1
 
 #===================================AUXILIARY_FUNCTIONS=====================================
 
+def potentialV(x):
+    return x**4/4 - k*x**2/2
+
+def EquilibriumFunction(x):
+    ret = np.zeros(N, dtype = float)
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            if i != j:
+                sum += 1/np.abs(x[i] - x[j])
+        ret[i] = -N * (x[i]**3 - k * x[i]) + sum
+    return ret
+
+def EquilibriumFunctionJacobian(x):
+    ret = np.zeros((N,N), dtype = float)
+    for i in range(N):
+        sum = 0
+        for j in range(N):
+            if i > j:
+                ret[i][j] = -1/(x[i] - x[j])**2
+                sum += 1/(x[i] - x[j])**2
+            if i < j:
+                ret[i][j] = 1/(x[i] - x[j])**2
+                sum += -1/(x[i] - x[j])**2
+        ret[i][i] = -N * (3 * x[i]**2 - k) + sum
+    return ret
+
 def GenerateMatrix():
-    M = np.zeros((N,N), dtype = complex)
-    M = np.random.normal(0, 1/2, (N,N)) + 1j * np.random.normal(0, 1/2, (N,N))                     
-    M = ( M + M.conj().T ) / 2      
-    return M                                                                   
+    initialGuess = np.zeros(N, dtype = float)
+    for i in range(N):
+        if i < N/2:
+            initialGuess[i] = np.random.normal(-np.sqrt(k), 1)
+        else:
+            initialGuess[i] = np.random.normal(np.sqrt(k), 1)
+
+    sol  = scipy.optimize.root(EquilibriumFunction, initialGuess, jac=EquilibriumFunctionJacobian)     
+    diag = sol.x
+    diag.sort()
+    M = np.zeros((N,N), dtype=complex)
+    for i in range(N):
+        M[i][i] = diag[i]
+    return M                                                            
 
 def Potential(M):
     return np.linalg.matrix_power(M, 4)/4 - k * np.linalg.matrix_power(M, 2) / 2
 
-
-def LnAcceptanceProbabilityFunction(A,B):                                                           # acceptance probability is given as p(x_{n+1})/p(x_n)
-    tmp = np.real( np.trace( Potential(A) - Potential(B) ) )                                        # where x_n is matrix M at n-th step
+# acceptance probability is given as p(x_{n+1})/p(x_n)
+# where x_n is matrix M at n-th step
+def LnAcceptanceProbabilityFunction(A,B):                                                           
+    tmp = np.real( np.trace( Potential(A) - Potential(B) ) )                                        
     return - N * tmp
 
-def PlotMatrix(matrix):
+def PlotMatrix(matrix, title):
     ret = np.absolute(matrix)
     fig1,ax1 = plt.subplots()
     cax = ax1.matshow(ret)
+    ax1.set_title(title)
     fig1.colorbar(cax)
 
 #===================================METROPOLIS_STEP_FUNCTION=====================================
@@ -45,13 +85,11 @@ terminationCounter = 0
 counter = 0
 
 while(1):
-    counter += 1
-    #print(terminationCounter)
-    #print(counter)
-    if counter == 20000:                                                                            # TODO: find better way to determine when minimum is found
+# TODO: find better way to determine when minimum is found
+    if terminationCounter == 1000000:                                                                            
         break
-
     nextM = M.copy()
+    print(terminationCounter)
 
     for i in range(order):
         index1, index2 = np.random.randint(0, N, size = 2)
@@ -61,24 +99,30 @@ while(1):
 
     lnAcceptanceProbability = LnAcceptanceProbabilityFunction(nextM, M)
 
-    if lnAcceptanceProbability > 0 :                                                                # defined as it is now, lnAcceptanceProbability > 0 means 
-        M = nextM                                                                                   # nextM is nearer to the local minimum of potential V than M
-        terminationCounter = 0
-    else:
-        u = np.random.random()
-        if u > 0.5 :
-            M = nextM
+    u = np.random.random()
+    if u < min(1, np.exp(lnAcceptanceProbability)) :
+        M = nextM.copy()
+        counter += 1
+        #print(terminationCounter, counter)
     terminationCounter += 1
 
-(evals, U) = np.linalg.eigh(originalM)                                                              # U diagonalizes M
-(evals, V) = np.linalg.eigh(M)                                                                      # V diagonalizes nextM
+# V diagonalizes nextM, U diagonalizes M
+(evals, U) = np.linalg.eigh(originalM)
+(evals, V) = np.linalg.eigh(M)                                                                      
 
-Udag = np.transpose( np.matrix.conjugate(U) )                                                       # V in the basis where M is diagonal is of the form U.conj().T * V * U
+# V in the basis where M is diagonal is of the form U.conj().T * V * U
+Udag = np.transpose( np.matrix.conjugate(U) )                                                       
 rez = np.matmul( np.matmul(Udag, V), U)
 
 
 #######################################PLOTS#########################################
 
-PlotMatrix(rez)
+#PlotMatrix(originalM, 'Original matrix')
+PlotMatrix(rez, 'Transformation matrix')
+PlotMatrix(M, 'Final matrix')
+evals, _ = np.linalg.eigh(M)
+xlinsp = np.linspace(-1.3*min(evals), 1.3*max(evals))
+figh, axh = plt.subplots()
+axh.plot(xlinsp, [potentialV(t) for t in xlinsp])
+axh.scatter(evals, [potentialV(t) for t in evals], color='red')
 plt.show()
-input()
